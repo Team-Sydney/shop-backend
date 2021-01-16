42425fea27684fbd01efab450cab1f073069d56d
thisisomar <abusamro@sheridancollege.ca>
2021-01-16 15:59:14 -0500

    Auto increment missing on models

--- models/vehicle.model.js
+++ models/vehicle.model.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, Sequelize) => {
     const Vehicle = sequelize.define('Vehicles', {
       vid: {
         type: Sequelize.INTEGER,
+        autoIncrement: true,
         primaryKey: true
       },
       cid: {
