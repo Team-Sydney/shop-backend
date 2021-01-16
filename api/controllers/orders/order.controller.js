ed2ce6e76bc5024816eb5a23158d71646017c4ad
Andres Munevar <afmunevarm@hotmail.com>
2021-01-16 15:14:40 -0500

    Add order contoller and add bid to order model

--- api/controllers/orders/order.controller.js
+++ api/controllers/orders/order.controller.js
@@ -2,29 +2,114 @@ const db = require('../../../models');
 const Order = db.Order;
 
 class OrderController {
-    createOrder(req, res) {
-        const order = {
-          oid: req.body.oid,
-          cid: req.body.cid,
-          qrCode: req.body.qrCode
-        };
-    
-        Order.create(order)
-          .then(data => {
-            res.send(data);
-          }) 
-          .catch(err => {
-            res.status(500).send({
-              message: 
-                err.message || "An error occurred while creating the order."
-            })
+  createOrder(req, res) {
+      const order = {
+        oid: req.body.oid,
+        cid: req.body.cid,
+        bid: req.body.bid,
+        qrCode: req.body.qrCode
+      };
+  
+      Order.create(order)
+        .then(data => {
+          res.send(data);
+        }) 
+        .catch(err => {
+          res.status(500).send({
+            message: 
+              err.message || "An error occurred while creating the order."
+          })
+      })
+  }
+
+  findOne(req, res) {
+      const id = req.params.id;
+  
+      Order.findByPk(id)
+        .then(data => {
+          res.send(data);
         })
+        .catch(err => {
+          res.status(500).send({
+            message: "Unfortunately we were unable to retrieve this order."
+          });
+        });
     }
+  
+  findByBusiness(req, res) {
+      const id = req.body.id;
+      Order.findAll({ 
+        where: { bid: id } 
+      })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || 'Unfortunately we were unable to retrieve all orders for this business.'
+        });
+    });
+  }
 
-    
-    deleteOrder(req, res) {
-        const orderID = req.body.oid;
+  findByCustomer(req, res) {
+    const id = req.body.id;
 
-        // Order.deleteOrder
-    }
+    Order.findAll({
+      where: { cid: id }
+    })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || 'Unfortunately we were unable to retrieve all orders for this customer.'
+      })
+    });
+  }
+
+  findByQRCode(req, res) {
+    const qrCode = req.body.qrCode
+
+    Order.findOne({
+      where: { qrCode: qrCode }
+    })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || 'Unfortunately we were unable to retrieve all orders for this qr code.'
+      })
+    });
+  }
+  
+  delete(req, res) {
+    const id = req.body.id;
+
+    Order.destroy({
+        where: { oid: id }
+    })
+    .then(num => {
+        if (num == 1) {
+            res.send({
+                message: "The order was deleted successfully!"
+            });
+        } else {
+            res.send({
+                message: "Unfortunately this order could not be found, please double check the ID."
+            });
+        }
+    })
+    .catch(err => {
+        console.log(id)
+        res.status(500).send({
+            message: "Unable to delete this order."
+        })
+    })
+  }
 }
+
+module.exports = OrderController;
---
+++ routes/order.routes.js
@@ -0,0 +1,22 @@
+const CategoryController = require('../api/controllers/categories/category.controller.js');
+const OrderController = require('../api/controllers/orders/order.controller.js');
+const controller = new OrderController();
+
+module.exports = (app) => {
+  
+  let router = require("express").Router();
+
+  router.post("/", controller.createOrder);
+
+  router.get("/:id", controller.findOne);
+
+  router.get("/business/:id", controller.findByBusiness); //Only available for businesses
+
+  router.get("/customer/:id", controller.findByCustomer);
+
+  router.get("/qr/:id", controller.findByQRCode);
+
+  router.delete("/:id", controller.delete);
+
+  app.use('/api/orders', router);
+}
