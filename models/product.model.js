973d7b5899afb67bd86b4df4dd5d9b4581741ef6
thisisomar <abusamro@sheridancollege.ca>
2021-01-16 13:35:27 -0500

    Proper type for Category ID on Product model

--- models/product.model.js
+++ models/product.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING
     },
     catid: {
-      type: Sequelize.BOOLEAN
+      type: Sequelize.INTEGER
     },
     quantity: {
       type: Sequelize.INTEGER
