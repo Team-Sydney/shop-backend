bb74bb3c52c0f3e5e31c7a6421539c8499d8d930
nichejambinn <benjaminrobininch@gmail.com>
2021-01-17 07:23:13 -0500

    cloud upload logs

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -9,6 +9,8 @@ module.exports = (app) => {
 
   router.get("/:id", controller.findOne);
 
+  router.get("/", controller.findAll);
+
   router.get("/user/:id", controller.findByUID);
 
   app.use('/api/customers', router);
