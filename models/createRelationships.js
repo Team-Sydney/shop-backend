38b3e9fa615509675019a6be34cf7fb8ce901e1f
thisisomar <abusamro@sheridancollege.ca>
2021-01-17 06:45:13 -0500

    Reverted back to flexible environment for GCP

--- models/createRelationships.js
+++ models/createRelationships.js
@@ -2,17 +2,12 @@ function createRelationships(sequelize) {
   const { Orders, Products, Businesses, OrderProduct, Customers, Vehicles } = sequelize.models;
 
   Products.belongsToMany(Orders, { through: OrderProduct });
-  Orders.hasMany(Products);
   
   Customers.hasMany(Vehicles, {foreignKey: 'cid'});
   Customers.hasMany(Orders, {foreignKey: 'cid'});
 
   Businesses.hasMany(Orders, { foreignKey: 'bid' });
   Businesses.hasMany(Products, { foreignKey: 'bid' });
-
-  // OrderProduct.belongsTo(Orders);
-  // OrderProduct.belongsTo(Products);
-
 }
 
 module.exports = { createRelationships }
