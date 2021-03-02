b148b42b808de1ab408bcf75ca4f70313df0aafc
Andres Munevar <afmunevarm@hotmail.com>
2021-03-01 19:10:03 -0500

    Added relationships

--- models/createRelationships.js
+++ models/createRelationships.js
@@ -1,15 +1,25 @@
 function createRelationships(sequelize) {
-  const { Orders, Products, Businesses, OrderProduct, Customers, Vehicles } = sequelize.models;
+    const { Orders, Products, Businesses, OrderProduct, Customers, Vehicles, Employees, Categories } = sequelize.models;
 
-  Products.belongsToMany(Orders, { through: OrderProduct });
-  
-  Customers.hasMany(Vehicles, {foreignKey: 'customerId'});
-  Customers.hasMany(Orders, {foreignKey: 'customerId'});
+    Customers.hasMany(Vehicles, { foreignKey: 'customerId' });
+    Customers.hasMany(Orders, { foreignKey: 'customerId' });
 
-  Vehicles.belongsTo(Customers, {foreignKey: 'customerId'});
+    Orders.belongsTo(Customers, { foreignKey: 'customerId' });
+    Orders.belongsTo(Businesses, { foreignKey: 'businessId' });
 
-  Businesses.hasMany(Orders, { foreignKey: 'businessId' });
-  Businesses.hasMany(Products, { foreignKey: 'businessId' });
+    Vehicles.belongsTo(Customers, { foreignKey: 'customerId' });
+
+    Businesses.hasMany(Orders, { foreignKey: 'businessId' });
+    Businesses.hasMany(Products, { foreignKey: 'businessId' });
+    Businesses.hasMany(Employees, { foreignKey: 'businessId' });
+
+    Employees.belongsTo(Businesses, { foreignKey: 'businessId' });
+
+    Products.hasMany(Categories, { foreignKey: 'productId' });
+    Products.belongsTo(Businesses, { foreignKey: 'businessId' });
+    Products.belongsToMany(Orders, { through: OrderProduct });
+
+    Categories.hasMany(Products, { foreignKey: 'productId' });
 }
 
 module.exports = { createRelationships }
