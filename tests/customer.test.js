2663b56707a600269b75f964f26f096cbfdc26bf
Andres Munevar <afmunevarm@hotmail.com>
2021-03-03 23:40:16 -0500

    Setting up jest and supertest

--- tests/customer.test.js
+++ tests/customer.test.js
@@ -1,9 +1,10 @@
-// const request = require('supertest')
-// const app = require('../app')
-// describe('Get Customers', () => {
-//     it('should retrive all customers', async() => {
-//         const res = await request(app).get('/api/customers');
-//         expect(res.statusCode).toEqual(200);
-//         expect(res.body).toHaveProperty('customer');
-//     })
-// })
+const request = require('supertest');
+const app = require('./../app');
+
+describe('GET /api/customers', () => {
+  it('Responds with json of all customers', async () => {
+    const res = await request(app)
+      .get('/api/customers');
+      expect(res.statusCode).toEqual(200);
+  });
+});
