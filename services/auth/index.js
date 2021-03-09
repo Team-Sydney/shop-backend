c6b812e43ea0b63132a92a63c283b25c7e08e3ff
thisisomar <abusamro@sheridancollege.ca>
2021-03-09 16:58:31 -0500

    Typos - woops

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,6 +1,6 @@
 module.exports = { 
   getAuthToken(req, res, next) {
-      return require('./getAuthToken.js')(req, res, next);
+    return require('./getAuthToken.js')(req, res, next);
   },
   checkIfAuthenticated(req, res, next) {
     return require('./checkIfAuthenticated.js')(req, res, next);
