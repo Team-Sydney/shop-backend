263388470ee6e87ddd68e5ea7bd789cd51d6531c
Jacob Brasil <jnxtbrasil@gmail.com>
2021-04-12 16:25:23 -0400

    Fixed Config for GAE & Local

--- config/config.js
+++ config/config.js
@@ -1,6 +1,20 @@
 const fs = require('fs');
 
 module.exports = {
+  production: {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync('./certs/cc-ca.crt').toString()
+      }
+    }
+  },
   development: {
     dialect: "postgres",
     host: process.env.DB_HOST,
